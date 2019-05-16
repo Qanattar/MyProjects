54a09979b91a9f80561429a77c924a009242e4ec
Kanat <kanat1406@gmail.com>
2019-05-16 16:29:53 +0600

    'upd'

---
+++ LilGame/LilGame/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace LilGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.hero = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bullet = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.bullet2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.bullet3 = new System.Windows.Forms.PictureBox();
+            this.coin = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.hero)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hero
+            // 
+            this.hero.BackColor = System.Drawing.Color.Transparent;
+            this.hero.Image = ((System.Drawing.Image)(resources.GetObject("hero.Image")));
+            this.hero.Location = new System.Drawing.Point(379, 409);
+            this.hero.Name = "hero";
+            this.hero.Size = new System.Drawing.Size(50, 50);
+            this.hero.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.hero.TabIndex = 0;
+            this.hero.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(320, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bullet
+            // 
+            this.bullet.Image = ((System.Drawing.Image)(resources.GetObject("bullet.Image")));
+            this.bullet.Location = new System.Drawing.Point(338, 0);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(25, 25);
+            this.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bullet.TabIndex = 2;
+            this.bullet.TabStop = false;
+            this.bullet.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(473, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // bullet2
+            // 
+            this.bullet2.Image = ((System.Drawing.Image)(resources.GetObject("bullet2.Image")));
+            this.bullet2.Location = new System.Drawing.Point(491, 0);
+            this.bullet2.Name = "bullet2";
+            this.bullet2.Size = new System.Drawing.Size(25, 25);
+            this.bullet2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bullet2.TabIndex = 4;
+            this.bullet2.TabStop = false;
+            this.bullet2.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(164, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // bullet3
+            // 
+            this.bullet3.Image = ((System.Drawing.Image)(resources.GetObject("bullet3.Image")));
+            this.bullet3.Location = new System.Drawing.Point(182, 0);
+            this.bullet3.Name = "bullet3";
+            this.bullet3.Size = new System.Drawing.Size(25, 25);
+            this.bullet3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bullet3.TabIndex = 6;
+            this.bullet3.TabStop = false;
+            this.bullet3.Visible = false;
+            // 
+            // coin
+            // 
+            this.coin.Image = ((System.Drawing.Image)(resources.GetObject("coin.Image")));
+            this.coin.Location = new System.Drawing.Point(663, 387);
+            this.coin.Name = "coin";
+            this.coin.Size = new System.Drawing.Size(20, 20);
+            this.coin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.coin.TabIndex = 7;
+            this.coin.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(670, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Your Score :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(756, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.coin);
+            this.Controls.Add(this.bullet3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.bullet2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.bullet);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.hero);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.hero)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox hero;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox bullet;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox bullet2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox bullet3;
+        private System.Windows.Forms.PictureBox coin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LilGame/LilGame/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LilGame
+{
+    public partial class Form1 : Form
+    {
+        #region  variables
+        int speed = 10;
+        int score = 0;
+        int bulletSpeed ;
+        bool moveRight = false;
+        bool moveLeft = false;
+        bool moveUp = false;
+        bool moveDown = false;
+        #endregion
+
+        Random random = new Random(DateTime.Now.Second);
+
+        public Form1()
+        {
+            InitializeComponent();
+            Point p = new Point
+            {
+                X = random.Next(100, 500),
+                Y = random.Next(150, 350)
+            };
+            bulletSpeed = random.Next(14,20);
+            coin.Location = new Point(p.X, p.Y);
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | 
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+
+        }
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D) moveRight = true;
+            if (e.KeyCode == Keys.A) moveLeft = true;
+            if (e.KeyCode == Keys.W) moveUp = true;
+            if (e.KeyCode == Keys.S) moveDown = true;
+
+            if (e.KeyCode == Keys.Escape) Application.Exit();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D) moveRight = false;
+            if (e.KeyCode == Keys.A) moveLeft = false;
+            if (e.KeyCode == Keys.W) moveUp = false;
+            if (e.KeyCode == Keys.S) moveDown = false;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (moveRight)
+            {
+                hero.Left += speed;
+            }
+            if (moveLeft)
+            {
+                hero.Left -= speed;
+            }
+            if (moveUp)
+            {
+                hero.Top -= speed;
+            }
+            if (moveDown)
+            {
+                hero.Top += speed;
+            }
+            #region bullets
+            bullet.Top += bulletSpeed;
+            if (bullet.Top > 50) bullet.Visible = true;
+            if (bullet.Top >499)
+            {
+                bullet.Top = random.Next(0,30);
+                bullet.Visible = false;
+            }
+            bulletSpeed = random.Next(14, 20);
+            bullet2.Top += bulletSpeed;
+            if (bullet2.Top > 50) bullet2.Visible = true;
+            if (bullet2.Top > 499)
+            {
+                bullet2.Top = random.Next(0, 30);
+                bullet2.Visible = false;
+            }
+            bulletSpeed = random.Next(14, 20);
+            bullet3.Top += bulletSpeed;
+            if (bullet3.Top > 50) bullet3.Visible = true;
+            if (bullet3.Top > 499)
+            {
+                bullet3.Top = random.Next(0, 30);
+                bullet3.Visible = false;
+            }
+            #endregion
+            
+            CheckCollision();
+            Refresh();
+        }
+
+        private void GenerateCoin()
+        {
+            Point p = new Point
+            {
+                X = random.Next(100, 500),
+                Y = random.Next(150, 350)
+            };
+            coin.Location = new Point(p.X, p.Y);
+        }
+
+        private void CheckCollision()
+        {
+            if (bullet.Top >= hero.Top - 25 && bullet.Top <= hero.Top + 25 && bullet.Left >= hero.Left && bullet.Left <= hero.Left + 25)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("GAME OVER");
+            }
+            if (bullet2.Top >= hero.Top - 25 && bullet2.Top <= hero.Top + 25 && bullet2.Left >= hero.Left && bullet2.Left <= hero.Left + 25)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("GAME OVER");
+            }
+            if (bullet3.Top >= hero.Top - 25 && bullet3.Top <= hero.Top + 25 && bullet3.Left >= hero.Left && bullet3.Left <= hero.Left + 25)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("GAME OVER");
+            }
+            if (coin.Top >= hero.Top - 25 && coin.Top <= hero.Top + 25 && coin.Left >= hero.Left && coin.Left <= hero.Left + 25)
+            {
+                GenerateCoin();
+                score++;
+                label2.Text = score.ToString();
+            }
+        }
+    }
+}
